7902eed729918fb0b29ec4d3f8ba60729505f020
Michał Walewski <pelikan2333@proton.me>
2025-02-14 21:56:24 +0100

    first commit: add files

---
+++ nvim/.config/nvim/lua/community.lua
@@ -0,0 +1,18 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.comment" },
+  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.git" },
+  { import = "astrocommunity.markdown-and-latex" },
+  { import = "astrocommunity.pack.lua" },
+}
